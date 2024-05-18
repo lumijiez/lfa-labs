cadedc6d604cc4afa38a5149008eca37c1ed5272
Daniel <danthevip@gmail.com>
2024-05-18 21:34:34 +0300

    fixed output for lab6

--- src/ParserAST/MainParser.java
+++ src/ParserAST/MainParser.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class MainParser {
     public static void main(String[] args) {
-        String input = "(3 + 5) * sqrt(16) + powtwo(5)";
+        String input = "(3 + 5) * sqrt(16) + powtwo(5) + loge(10)";
         List<Token> tokens = Lexer.tokenize(input);
         System.out.println("Tokens:");
         tokens.forEach(System.out::println);
